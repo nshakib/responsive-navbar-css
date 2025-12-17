b671b0909331e8b04cbe3bbf2a9e16a36958bbf1
nshakib <nazmus.shakib.cse@gmail.com>
2025-12-17 16:49:12 +0600

    responsive navbar demo with css

---
+++ script.js
@@ -0,0 +1,17 @@
+const header = document.querySelector("header");
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 160);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlinks = document.querySelector('.navlinks');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlinks.classList.toggle('open');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlinks.classList.remove('open');
+}
